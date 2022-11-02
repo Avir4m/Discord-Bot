3b547973b26ea12751b7f09a66290cf9ab8d714b
unknown <avir4m@gmail.com>
2022-11-02 16:48:53 +0200

    added math problems questions

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -8,10 +8,10 @@ class commands(commands.Cog):
 
     @commands.command()
     async def meme(self, ctx):
-        result = requests.get("https://meme-api.herokuapp.com/gimme").json()
+        meme = requests.get("https://meme-api.herokuapp.com/gimme").json()
         embed = discord.Embed()
-        embed.set_image(url=result["url"])
-        embed.add_field(name="Original Post", value=result["postLink"], inline=True)
+        embed.set_image(url=meme["url"])
+        embed.add_field(name="Original Post", value=meme["postLink"], inline=True)
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -20,6 +20,31 @@ class commands(commands.Cog):
         first_digit = int(number / 10)
         second_digit = number % 10 * 10
         await ctx.send(first_digit + second_digit)
+
+    @commands.command()
+    async def math(self, ctx):
+
+        problem = requests.get("https://x-math.herokuapp.com/api/random?max=15&negative=1").json()
+        answer = problem["answer"]
+        await ctx.send(problem["expression"])
+
+        def check(m):
+            return len(m.content) >= 1 and m.author != self.bot.user
+            
+        ALLOWED_TRIES = 3
+        a = 0
+
+        while a <= ALLOWED_TRIES:
+            user_answer = await self.bot.wait_for("message", check=check)
+            if int(user_answer.content) == int(answer):
+                await ctx.send("Your answer is correct!")
+                break
+            else:
+                await ctx.send(f"Your answer is wrong, you have {ALLOWED_TRIES-a} tries left.")
+                a += 1
+
+
+
         
 
 async def setup(bot):
